4119603b330578de84ae52aa746f104f2675d977
Mateus Gomes <mateus.gs1999@gmail.com>
2021-04-19 20:24:26 -0300

    componentes do template

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -1,11 +1,13 @@
-import './Nav.css'
+import './Main.css'
 import React from 'react'
 import Header from './Header'
 
 export default props => 
     <React.Fragment>
-        <Header />
-        <main className="content">
-            Conteúdo
+        <Header {...props} /> 
+        <main className="content container-fluid">
+            <div className="p-3 mt-3">
+                {props.children}
+            </div>
         </main>
     </React.Fragment>
